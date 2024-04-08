b7b7599813a05d8ef758e79fd43c7154c40f2bf5
this-scott <this_scott@outlook.com>
2024-04-08 19:52:15 -0400

    added csv creation for weeks table

--- transformations.py
+++ transformations.py
@@ -2,8 +2,8 @@ import pandas as pd
 import numpy as np
 
 
-#assuming these are 18 week seasons, tw=currweeks
-def add_season(df, csv, tw):
+#assuming these are 18 week seasons, tw=currweeks, wdf = week df
+def add_season(df, csv, tw, wdf, snum):
     df2 = pd.read_csv(csv)
     df2 = df2.drop('#',axis=1)
     nw = 0
@@ -13,13 +13,15 @@ def add_season(df, csv, tw):
             nw +=1
             newcol = oc+tw
             df2 = df2.rename(columns={col:newcol})
+            new_data= {'Id': newcol, 'Week_Num':nw, 'Season':snum}
+            wdf.loc[len(wdf)] = new_data
         except ValueError:
             print("Valueerror"+col)
             continue
     df = pd.merge(df, df2, on=["Player", "Pos", "Team"], how='outer')
     pd.set_option('display.max_columns', None)
-    print(df.head())
-    return df, tw+nw
+    print(wdf.head(25))
+    return df, tw+nw, wdf
 
 #gonna have to fill by hand
 def add_opp_cols(df):
@@ -32,11 +34,17 @@ def add_opp_cols(df):
             print("Valueerror"+col)
             continue
     return df
+
 y2020 = pd.read_csv("project_datasets/FantasyPros_Fantasy_Football_Points_QB_2020.csv")
 y2020 = y2020.drop('#',axis=1)
+weeks = pd.read_csv("project_datasets/Weeks.csv")
+
 totalweeks = 17
-y2020, totalweeks = add_season(y2020, "project_datasets/FantasyPros_Fantasy_Football_Points_QB_2021.csv", totalweeks)
-y2020, totalweeks = add_season(y2020, "project_datasets/FantasyPros_Fantasy_Football_Points_QB_2022.csv", totalweeks)
-y2020, totalweeks = add_season(y2020, "project_datasets/FantasyPros_Fantasy_Football_Points_QB_2023.csv", totalweeks)
+#games per season
+gps = [17]
+y2020, totalweeks, weeks= add_season(y2020, "project_datasets/FantasyPros_Fantasy_Football_Points_QB_2021.csv", totalweeks, weeks, 2021)
+y2020, totalweeks, weeks = add_season(y2020, "project_datasets/FantasyPros_Fantasy_Football_Points_QB_2022.csv", totalweeks, weeks, 2022)
+y2020, totalweeks,weeks = add_season(y2020, "project_datasets/FantasyPros_Fantasy_Football_Points_QB_2023.csv", totalweeks, weeks, 2023)
 y2020 = add_opp_cols(y2020)
 y2020.to_csv("PPpW.csv", index=True)
+weeks.to_csv("project_datasets/Weeks.csv", index=False)
