5d8f63025655c4ae8db77aedfc4222e3eb09e60a
this-scott <this_scott@outlook.com>
2024-04-11 23:40:15 -0400

    Added cool queries and screenshots

--- queries.sql
+++ queries.sql
@@ -1,3 +1,17 @@
-SELECT * FROM `PPpW` WHERE FScore > 30;
+/* Games where the QB earned > 40 fantasy points, these are rare*/
+SELECT * FROM `Fact` WHERE points > 40;
 
-SELECT * FROM `PPpW` WHERE Pid = 20;
+/* C.J. Stroud(him) Game list */
+SELECT * FROM `Fact` WHERE Pid = 20;
+
+/* Dak Prescott lifetime points */
+SELECT * FROM `Lifetime` WHERE pid = 30;
+
+/* Finding out who has the most 40 point games(Lamar Jackson) */
+SELECT pid, COUNT(pid) AS forty_point_games FROM `Fact` WHERE points > 40 GROUP BY pid ORDER BY forty_point_games DESC LIMIT 1;
+
+/* Finding the highest average points scored(Josh Allen) */
+SELECT pid, AVG(points) AS avg_points FROM `Fact` GROUP BY pid ORDER BY avg_points DESC LIMIT 1;
+
+/* Figuring out which team hinders QB performance the most(which team QBs score on least) (N.O. Saints) */
+SELECT * FROM `Vs` ORDER BY points ASC LIMIT 1;
